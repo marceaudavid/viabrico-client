061d310964a15ffb6830f79eceb3ee1eef745a70
corentin <corentin.guillard@ynov.com>
2019-04-30 11:12:54 +0200

    Start nav for login

---
+++ src/components/nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav">
+    <img class="logo" src="../assets/icons/close.svg" alt="logo">
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Nav",
+  data() {
+    return {
+      display: true
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.nav {
+  width: 100%;
+  background-color: black;
+}
+.logo {
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,20 @@
 <template>
+  <Nav/>
   <Form name="login"/>
 </template>
 
 <script>
 import Form from "@/components/Form.vue";
+import Nav from "@/components/nav.vue";
+
 export default {
   name: "Login",
+  props: {
+    name: String
+  },
   components: {
-    Form
+    Form,
+    Nav
   }
 };
 </script>
