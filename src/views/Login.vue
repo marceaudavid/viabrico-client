5d0d331e3231aba3143ca7e5d7b4caf9de39dd3e
corentin <corentin.guillard@ynov.com>
2019-04-30 11:28:23 +0200

    Delete navlogin

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,13 @@
 <template>
-  <Nav name="nav"/>
   <Form name="login"/>
 </template>
 
 <script>
 import Form from "@/components/Form.vue";
-import Nav from "@/components/NavLogin.vue";
-
 export default {
   name: "Login",
-  props: {
-    name: String
-  },
   components: {
-    Form,
-    Nav
+    Form
   }
 };
 </script>
