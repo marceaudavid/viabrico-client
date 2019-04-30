351e59ef62dbc9d387db1f95e143166b5244fe57
marceaudavid <marceau.david@ynov.com>
2019-04-30 08:17:52 +0200

    Login and register

---
+++ src/components/Popup.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="display" class="popup">
+    <p class="popup-text">{{error}}</p>
+    <img
+      v-on:click="togglePopup"
+      class="popup-icon"
+      src="@/assets/icons/close.svg"
+      alt="close popup"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Popup",
+  props: {
+    error: String
+  },
+  data() {
+    return {
+      display: true
+    };
+  },
+  methods: {
+    togglePopup() {
+      this.display = !this.display;
+    }
+  }
+};
+</script>
+
+<style>
+.popup {
+  width: 250px;
+  min-height: 25px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 5px 0px;
+  padding: 0px 5px;
+  border: 1px solid #2c3e50;
+  border-radius: 5px;
+  background-color: #e74c3c;
+}
+.popup-text {
+  margin: 0px;
+  font-family: "Montserrat", Courier, monospace;
+  font-size: 1em;
+  color: #2c3e50;
+}
+.popup-icon {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Submit.vue
@@ -0,0 +1,48 @@
+<template>
+  <button v-if="!loading" class="submit" type="submit">Submit</button>
+  <button v-else class="submit" type="submit">
+    <div class="loader"></div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Submit",
+  props: {
+    loading: Boolean
+  },
+  methods: {
+    isLoading() {}
+  }
+};
+</script>
+
+<style>
+.submit {
+  margin: 20px 0px;
+  cursor: pointer;
+  font-family: "Lato", Courier, monospace;
+  font-size: 1em;
+  border: 1px solid #2c3e50;
+  border-radius: 5px;
+  color: #2c3e50;
+}
+
+.loader {
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #2c3e50;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
